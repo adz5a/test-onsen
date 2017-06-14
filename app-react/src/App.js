94c198a59f37e01d0ea3ad530faa49a86db1e9b9
antoine zimmermann <antoine.zimmermann@mobilespot.com>
2017-06-14 22:31:47 +0200

    add menu for all pages

--- app-react/src/App.js
+++ app-react/src/App.js
@@ -12,6 +12,14 @@ import {
     BrowserRouter as Router,
     Route
 } from "react-router-dom";
+import {
+    Splitter,
+    SplitterSide,
+    SplitterContent,
+    Page as OnsenPage,
+    List,
+    ListItem
+} from "react-onsenui";
 
 
 
@@ -21,21 +29,43 @@ class App extends Component {
             <Router
                 forceRefresh={false}
             >
-                <div>
-                    <Route 
-                        path="/"
-                        exact={true}
-                        component={HomePage}
-                    />
-                    <Route 
-                        component={LoginPage}
-                        path="/login"
-                    />
-                    <Route 
-                        component={TodoListPage}
-                        path="/todolist"
-                    />
-            </div>
+                <Splitter>
+                    <SplitterSide
+                        style={{
+                            boxShadow: '0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23)'
+                        }}
+                        side='left'
+                        width={200}
+                        collapse={true}
+                        isSwipeable={true}
+                        isOpen={true}
+                    >
+                        <OnsenPage>
+                            <List 
+                                dataSource={["Profile", "Settings"]}
+                                renderRow={
+                                    (data, index) => <ListItem key={index}>{data}</ListItem>
+                                        
+                                }
+                            />
+                        </OnsenPage>
+                    </SplitterSide>
+                    <SplitterContent>
+                        <Route 
+                            path="/"
+                            exact={true}
+                            component={HomePage}
+                        />
+                        <Route 
+                            component={LoginPage}
+                            path="/login"
+                        />
+                        <Route 
+                            component={TodoListPage}
+                            path="/todolist"
+                        />
+                    </SplitterContent>
+            </Splitter>
         </Router>
         );
     }
