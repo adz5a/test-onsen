6f22c85fed88ddbdb6004f679d6b45e58e6ba333
antoine zimmermann <antoine.zimmermann@mobilespot.com>
2017-06-17 20:29:45 +0200

    add tests for page

--- app-react/src/views/Page.test.js
+++ app-react/src/views/Page.test.js
@@ -4,7 +4,13 @@ import reactTapEventPlugin from "react-tap-event-plugin";
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 import { Page } from "./Page";
 import renderer from "react-test-renderer";
-
+import {
+    shallow,
+    mount
+} from "enzyme";
+import AppBar from "material-ui/AppBar";
+import BackIcon from "material-ui/svg-icons/hardware/keyboard-arrow-left";
+import IconButton from "material-ui/IconButton";
 
 // necessary for the onTap prop
 reactTapEventPlugin();
@@ -40,6 +46,36 @@ it("matches snapshot", () => {
     ).toJSON();
 
     expect(tree).toMatchSnapshot();
+
+
+});
+
+it("render an app bar", () => {
+
+    const wrapper = shallow(
+            <Page />
+    );
+
+    expect(wrapper.find(AppBar).length)
+        .toBe(1);
+
+});
+
+it("has a back button when the props is to true", () => {
+
+    const wrapper = mount(
+        <MuiThemeProvider>
+            <Page 
+                hasBackButton={true}
+            />
+        </MuiThemeProvider>,
+    );
+
+    expect(wrapper.find(IconButton).length)
+        .toBe(2);
+
+    expect(wrapper.find(BackIcon).length)
+            .toBe(1);
 
 
 });
