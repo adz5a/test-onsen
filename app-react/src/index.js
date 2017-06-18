0e877bf238f9c3b5e0d9bfe85a2c4faa9135bc7e
antoine zimmermann <antoine.zimmermann@mobilespot.com>
2017-06-18 15:20:52 +0200

    add simple reset file to remove uneeded stuff

--- app-react/src/index.js
+++ app-react/src/index.js
@@ -24,6 +24,7 @@ import {
 } from "./env";
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 import reactTapEventPlugin from "react-tap-event-plugin";
+import "./reset.css";
 
 reactTapEventPlugin();
 
